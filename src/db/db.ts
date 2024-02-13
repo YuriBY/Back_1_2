475ec3b4e27f454aab70d068bb65b654baa33f12
YuriBY <vv0325056@gmail.com>
2024-02-13 23:41:41 +0300

    add vercel istr

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,13 +2,6 @@ import { BlogType } from "../models/blogs";
 import { PostType } from "../models/postType";
 
 export const db: { blogs: BlogType[]; posts: PostType[] } = {
-  blogs: [
-    {
-      id: "123",
-      name: "555",
-      description: "qnsvdlbvfl",
-      websiteUrl: "https://google.com",
-    },
-  ],
+  blogs: [],
   posts: [],
 };
